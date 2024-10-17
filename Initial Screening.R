737e82a606d04ffe76fe5bb919f378f5eead35d6
Julia Rohrer <juliarohrer@Air-von-Julia.fritz.box>
2024-10-17 16:52:19 +0200

    Read in the 2010 data and did some quick checks regarding how the satisfaction values changed

--- Initial Screening.R
+++ Initial Screening.R
@@ -6,15 +6,7 @@
 ######################
 # Jugendstudie 2023
 ######################
-
-load("Files/jugend2023.rds") # I cannot open this file
-
-# Lets check another one
-library(haven)
-jugend2023 <- read_sav("Files/jugend2023.sav")
-View(jugend2023)
-names(jugend2023)
-# This one works
+jugend2023 <- readRDS("Files/jugend2023.rds")
 
 # Wie zufrieden mit Leben insgesamt?
 # 1: sehr zufrieden, zufrieden, teils/teils, unzufrieden, sehr unzufrieden
@@ -41,4 +33,37 @@ rel_weighted_freq
 
 # weights dont seem to make a big difference here
 
-# test
+######################
+# Jugendstudie 2010
+######################
+jugend2010 <- readRDS("Files/umfj2010.rds")
+schul2010 <- readRDS("Files/umfschule2010.rds") # Whats the difference here
+
+erwerb2010 <- readRDS("Files/umfj2010_erwerbverlauf.rds")
+
+# Wie zufrieden zur Zeit
+table(jugend2010$j02a)
+
+table(jugend2010$geschl) # 1 maennlich, 2 weiblich
+
+prop.table(table(jugend2010$j02a[jugend2010$geschl == 1])) # maennlich
+prop.table(table(jugend2010$j02a[jugend2010$geschl == 2])) # weiblich
+
+mean(jugend2010$j02a[jugend2010$geschl == 1], na.rm = TRUE) # maennlich, 2010
+mean(jugend2010$j02a[jugend2010$geschl == 2], na.rm = TRUE) # weiblich, 2010
+
+mean(jugend2023$f02a[jugend2023$geschl == 1], na.rm = TRUE) # maennlich, 2010
+mean(jugend2023$f02a[jugend2023$geschl == 2], na.rm = TRUE) # weiblich, 2010
+
+sd(jugend2010$j02a, na.rm = TRUE) # 0.87
+sd(jugend2023$f02a, na.rm = TRUE) # 0.85
+
+# Diff girls
+(mean(jugend2010$j02a[jugend2010$geschl == 2], na.rm = TRUE) - mean(jugend2023$f02a[jugend2023$geschl == 2], na.rm = TRUE))/
+  0.86
+# - 0.3
+
+# Diff boys
+(mean(jugend2010$j02a[jugend2010$geschl == 1], na.rm = TRUE) - mean(jugend2023$f02a[jugend2023$geschl == 1], na.rm = TRUE))/
+  0.86
+# 0.13
