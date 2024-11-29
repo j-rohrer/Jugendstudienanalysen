6d3db15438ce52cde14a3b1bebc51cf64487e72b
Julia Rohrer <juliarohrer@MacBook-Air-von-Julia.local>
2024-11-29 15:33:56 +0100

    Add ids to initial data cleaning, did some analysis to check how schools with tablets are different, next up: map of where the students come from

--- 08_Find out whats different about the schools without tablets.R
+++ 08_Find out whats different about the schools without tablets.R
@@ -14,8 +14,10 @@ load("prep.RData")
 have_tablet <- unique(youth2023$schule_amt51[youth2023$source == "online"])
 have_paper <- unique(youth2023$schule_amt51[youth2023$source == "paper"])
 
-prop.table(table(youth2023$schooltype[youth2023$schule_amt51 %in% have_tablet]))
-prop.table(table(youth2023$schooltype[youth2023$schule_amt51 %in% have_paper]))
+prop.table(table(youth2023$schooltype[youth2023$schule_amt51 %in% have_tablet & 
+                                        youth2023$rawid %in% combined$id]))
+prop.table(table(youth2023$schooltype[youth2023$schule_amt51 %in% have_paper & 
+                                        youth2023$rawid %in% combined$id]))
 # 1 Mittelschule
 # 2 Gymnasium
 # 3 Berufs-/Fachoberschule
@@ -24,10 +26,7 @@ prop.table(table(youth2023$schooltype[youth2023$schule_amt51 %in% have_paper]))
 # With tablet: More Mittelschule
 # With Paper: More Gymnasium, Berufs/Fachoberschule, Förderschule
 
-# no, lets work from the combined data
 
-prop.table(table(combined$schooltype[combined$source == "online"]))
-prop.table(table(combined$schooltype[combined$source == "paper"]))
 
 # does the interaction disappear if we split by schooltype
 
@@ -39,3 +38,55 @@ summary(gym)
 
 # No: we have the pattern in both
 # so this aint it chief
+
+
+# migration background
+# this clearly does not vary systematically
+
+prop.table(table(youth2023$migback[youth2023$rawid %in% combined$id], youth2023$source[youth2023$rawid %in% combined$id]), margin = 2)
+
+
+# looks like the same age distribution
+summary(youth2023$age[youth2023$source == "paper" & youth2023$rawid %in% combined$id])
+summary(youth2023$age[youth2023$source == "online" & youth2023$rawid %in% combined$id])
+
+
+prop.table(table(youth2023$gender[youth2023$rawid %in% combined$id], youth2023$source[youth2023$rawid %in% combined$id]), margin = 2)
+# looks about the same
+
+
+
+# number of siblings
+prop.table(table(youth2023$f32_diff[youth2023$rawid %in% combined$id], youth2023$source[youth2023$rawid %in% combined$id]), margin = 2)
+# slightly larger families on paper
+table(youth2023$f32_diff)
+
+#################
+# to check
+# does the interaction disappear by family size?
+#################
+
+
+# I will have to clean up these
+prop.table(table(youth2023$plz[youth2023$rawid %in% combined$id], youth2023$source[youth2023$rawid %in% combined$id]), margin = 2)
+unique(youth2023$plz)
+table(youth2023$plz_diff)
+prop.table(table(youth2023$plz_diff[youth2023$rawid %in% combined$id], youth2023$source[youth2023$rawid %in% combined$id]), margin = 2)
+
+# more paper in plz diff 15, 16
+# I will have to plot a map or something
+
+# parental work
+prop.table(table(youth2023$f33a[youth2023$rawid %in% combined$id], youth2023$source[youth2023$rawid %in% combined$id]), margin = 2) # vater
+prop.table(table(youth2023$f33b[youth2023$rawid %in% combined$id], youth2023$source[youth2023$rawid %in% combined$id]), margin = 2) # mutter
+
+# looks approximately the same!
+
+table(youth2023$f33a) # Vater
+table(youth2023$f33b) # Mutter
+
+#################
+# next step
+#################
+
+# do a map to figure out where people are from
